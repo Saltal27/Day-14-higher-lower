4114f972affaf23f1a87c6701c78e2b5207f1577
Sal Tal <110280707+Saltal27@users.noreply.github.com>
2022-10-28 08:42:13 +0000

    final touches

--- main.py
+++ main.py
@@ -10,51 +10,68 @@ second_person = random.choice(data)
 a = ""
 b = ""
 
-def new_comparison():
-  global first_person
-  global second_person
-  global a
-  global b
-  
-  first_person = second_person
-  a = f"{first_person['name']}, {first_person['description']}, {first_person['country']}"
 
-  second_person = random.choice(data)
-  b = f"{second_person['name']}, {second_person['description']}, {second_person['country']}"
+def new_comparison():
+    global first_person
+    global second_person
+    global a
+    global b
+
+    first_person = second_person
+    a = f"{first_person['name']}, {first_person['description']}, {first_person['country']}"
+
+    second_person = random.choice(data)
+    b = f"{second_person['name']}, {second_person['description']}, {second_person['country']}"
 
 
 def compare(A, B):
-  a_folower = A['follower_count']
-  b_folower = B['follower_count']
-  if a_folower > b_folower:
-    return "a"
-  else:
-    return "b"
+    a_folower = A['follower_count']
+    b_folower = B['follower_count']
+    if a_folower > b_folower:
+        return "a"
+    else:
+        return "b"
+
 
 def evaluate():
-  global current_score
-  global end_of_game
-  
-  if who == compare(first_person, second_person):
-    current_score += 1
-  else:
-    end_of_game = True
+    global current_score
+    global end_of_game
 
+    if who == compare(first_person, second_person):
+        current_score += 1
+    else:
+        end_of_game = True
+
+
+again = True
 end_of_game = False
-while not end_of_game:
-  clear()
-  print(logo)
-  new_comparison()
-  
-  if current_score > 0:
-    print(f"You're right! Current score: {current_score}")
+while again:
+    while not end_of_game:
+        clear()
+        print(logo)
+        new_comparison()
 
-  print(f"Compare A: {a}")
-  print(vs)
-  print(f"Compare B: {b}")
-  who = input("Who has more followers? Type 'A' or 'B': ").lower()
-  evaluate()
+        if current_score > 0:
+            print(f"You're right! Current score: {current_score}")
 
-clear()
-print("Sorry, thats's wrong :(")
-print(f"Final score: {current_score}")
+        print(f"Compare A: {a}")
+        print(vs)
+        print(f"Compare B: {b}")
+        who = input("Who has more followers? Type 'A' or 'B': ").lower()
+        evaluate()
+
+    clear()
+    print("Sorry, thats's wrong :(")
+    print(f"Final score: {current_score}")
+
+    again = input(
+        "Do you wanna play another game of 'High / Low'? ツ\nType 'y' or 'n': ")
+    if again == 'y':
+        end_of_game = False
+        current_score = 0
+    elif again == 'n':
+        again = False
+        print("Understandable. Have a great day ╍●‿●╍")
+    else:
+        again = False
+        print("Invalid info!")
